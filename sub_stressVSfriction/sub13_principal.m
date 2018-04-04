9e72d69b08719e59d9abb8aed74d2e738767afe1
Jing-yao Chen <jchen478@wisc.edu>
2018-04-04 12:18:31 -0500

    Deleted sub14

--- sub_stressVSfriction/sub13_principal.m
+++ sub_stressVSfriction/sub13_principal.m
@@ -6,68 +6,14 @@ clc;
 clear;
 close all;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Define common parameters
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%{
-%%%%%%%%%%%%%%%%%%%%% U-shaped fibers %%%%%%%%%%%%%%%%%%%%%
-% fileNameArr = {'theta0'}; thetaArr = 0;
-% fileNameArr = {'theta1'}; thetaArr = 1;
-% fileNameArr = {'theta3'}; thetaArr = 3;
-fileNameArr = {'theta6'}; thetaArr = 6;
+% Define common parameters
+simulation_cases;
 
-nfibArr = [160 240 320 640 1280 3200 6400 10240 12800];
-lboxArr = [300 343.4 378 476.2 600 814.3 1026 1200 1293];
-muArr = [0 1 2 3 4 5 7 10 15 17 20 23];
+% data path
+dataPath = '../data_stressVSfriction/ClusterP/';
 
-muArr = [0 1 2 3 4 5 10 15 20];
-
-rpFiber = 75;
-colorArr = {rgb('DarkRed') rgb('Crimson') rgb('OrangeRed')...
-    rgb('Orange') rgb('Gold') rgb('Lime')...
-    rgb('Olive') rgb('DarkGreen') rgb('LightSkyBlue') ...
-    rgb('MediumBlue') rgb('Plum') rgb('Purple') };
-%}
-%%{
-%%%%%%%%%%%%%%%%%%%%% Helical fibers %%%%%%%%%%%%%%%%%%%%%
-nfibArr = [160 240  320 640 1280 3200 6400];
-lboxArr = [300 343.4 378 476.2 600 814.3  1026];
-muArr = [0 1 2 3 4 5 10 15 20];
-
-thetaArr = [3];
-fileNameArr = {'helical'};
-rpFiber = 75;
-colorArr = {rgb('DarkRed') rgb('Crimson') rgb('OrangeRed')...
-    rgb('Orange') rgb('Gold') rgb('Lime')...
-    rgb('DarkGreen') rgb('LightSkyBlue') rgb('Plum')};
-%}
-
-nTheta = length(thetaArr);
-nMu = length(muArr);
-nLbox = length(lboxArr);
-nNfib = length(nfibArr);
-muLegendArr = cell(nMu,1);
-thetaNfibLegendArr = cell(nTheta*nNfib,1);
-markersize = 50;
-
-for i=1:nMu
-    muLegendArr{i} = ['$\mu =$ ',num2str(muArr(i))];
-end
-if strcmpi(fileNameArr,'helical')
-    for i=1:nTheta
-        for j=1:nNfib
-            thetaNfibLegendArr{(i-1)*nNfib+j} = ['$(\theta_{eq},\phi_{eq},N_{fib}) =$ (0.8, 0.7, ',num2str(nfibArr(j)),')'];
-        end
-    end
-else
-    for i=1:nTheta
-        for j=1:nNfib
-            thetaNfibLegendArr{(i-1)*nNfib+j} = ['$(\theta_{eq},N_{fib}) =$ (0.',num2str(thetaArr(i)),', ',num2str(nfibArr(j)),')'];
-        end
-    end
-end
+% figure info
 figStart = 1;
-
 dataFile = cell(2,1);
 for ii=1:2
     dataFile{ii} = ['fig13_principal/',fileNameArr{1},'_nfib'];
@@ -77,14 +23,12 @@ for ii=1:2
 end
 dataFile{1} = [ dataFile{1},'principal_direction3'];
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%  Plot principal directions vs. strain
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-dataPath = '../data_stressVSfriction/ClusterP/';
+% array allocation
 N = zeros(nMu, nNfib); % number of flocs meeting criteria
 Np_avg = zeros(nMu, nNfib);
 Np_std = zeros(nMu, nNfib);
 
+%%  Plot principal directions vs. strain
 for j=1:nNfib
     figure('units','normalized','outerposition',[0.2 0.2 0.25 0.4])
     hold on
